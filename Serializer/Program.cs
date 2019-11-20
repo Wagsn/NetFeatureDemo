2cc8801359dfed6b0846b3a75f648298a282829e
Wagsn <wagsn@foxmail.com>
2019-11-21 00:15:23 +0800

    新增：序列化器，已测试JSON、XML、YAML的序列化与反序列化

---
+++ Serializer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Serializer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            YAML.YAMLTest.Test();
+            Console.WriteLine();
+            JSON.JSONTest.Test();
+            Console.WriteLine();
+            XML.XMLTest.Test();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Serializer/Props/PropertiesSerializerTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Serializer.Props
+{
+    public class PropertiesSerializerTest
+    {
+        public static void Test()
+        {
+
+        }
+    }
+}
