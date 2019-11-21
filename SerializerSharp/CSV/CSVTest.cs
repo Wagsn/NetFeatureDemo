cb3e12ed51c4a729747603c0d3514de0e8a9ea17
Wagsn <wagsn@foxmail.com>
2019-11-21 18:07:02 +0800

    添加CSV三方库测试

---
+++ SerializerSharp/CSV/CSVTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SerializerSharp
+{
+    internal class CSVTest
+    {
+        public static void Test()
+        {
+            // CsvHelper.CsvReader 只支持流
+            var source = "Wagsn,23,true\r\nBruce,25,true";
+            using (CsvHelper.CsvReader reader = new CsvHelper.CsvReader(new System.IO.StreamReader(ConvertHelper.ConvertToStream(source))))
+            {
+                if (reader.Read())
+                {
+                    foreach(var per in reader.GetRecords<Person>())
+                    {
+                        Console.WriteLine($"deserialize: name: {per?.Name}, age: {per?.Age}, sex: {per?.Sex}");
+                    }
+                }
+            }
+            //var path = "./CSV/person.csv";
+        }
+        public class Person
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public bool Sex { get; set; }
+        }
+    }
+}
---
+++ SerializerSharp/CSV/CsvSerializer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace SerializerSharp
+{
+    /// <summary>
+    /// CSV Serializer
+    /// </summary>
+    public class CsvSerializer : ISerializer
+    {
+        public string Name { get; set; } = "CsvHelper";
+
+        public TypeEntity Deserialize<TypeEntity>(string content)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TypeEntity Deserialize<TypeEntity>(Stream stream)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Serialize(object entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Serialize(object entity, Stream stream)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
