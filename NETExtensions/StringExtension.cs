7bf4d4345efe41863f013fac0a23353d6d674d31
Wagsn <wagsn@foxmail.com>
2019-11-22 18:01:33 +0800

    新增：字符串扩展

---
+++ NETExtensions/StringExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NETExtensions
+{
+    public static class StringExtension
+    {
+        public static System.IO.MemoryStream ToStream(this string source)
+        {
+            return new System.IO.MemoryStream(source.ToBytes());
+        }
+
+        public static byte[] ToBytes(this string source)
+        {
+            return Encoding.ASCII.GetBytes(source);
+        }
+    }
+}
